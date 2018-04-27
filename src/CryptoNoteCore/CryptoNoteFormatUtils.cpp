f99c937d67513ab1bdf12351ba44eeccbb99c9b1
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-04-27 23:19:17 +0430

    Anti ASIC variant

--- src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
@@ -504,7 +504,8 @@ bool get_block_longhash(cn_context &context, const Block& b, Hash& res) {
   } else {
     return false;
   }
-  cn_slow_hash(context, bd.data(), bd.size(), res);
+  const int cn_variant = b.majorVersion >= 4 ? b.majorVersion - 3 : 0;
+  cn_slow_hash(context, bd.data(), bd.size(), res, cn_variant);
   return true;
 }
 
