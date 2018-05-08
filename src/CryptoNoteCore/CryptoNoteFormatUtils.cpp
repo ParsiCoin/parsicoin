c79c1b336f8fc82d44db042e0b10ce4515fbb00c
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-05-08 21:12:47 +0430

    Update CryptoNoteFormatUtils.cpp

--- src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
@@ -503,7 +503,7 @@ bool get_block_longhash(cn_context &context, const Block& b, Hash& res) {
   } else {
     return false;
   }
-  const int cn_variant = b.majorVersion >= 3 ? b.majorVersion - 2 : 0;
+  const int cn_variant = b.majorVersion >= 4 ? b.majorVersion - 3 : 0;
   cn_slow_hash(context, bd.data(), bd.size(), res, cn_variant);
   return true;
 }
