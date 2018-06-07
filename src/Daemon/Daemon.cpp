ea7aecc4a38b77ac489e7db3ee72a0bf0fdaab1e
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-06-08 01:27:58 +0430

    Update Daemon.cpp

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2016, The Forknote developers
 // Copyright (c) 2016, The Karbowanec developers
+// Copyright (c) 2018, The ParsiCoin developers
 // This file is part of Bytecoin.
 //
 // Bytecoin is free software: you can redistribute it and/or modify
