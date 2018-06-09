0d18a7c1f681be9a6443c2623fc2a16cbdfa97db
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-06-09 17:36:50 +0430

    Update RpcServer.h

--- src/Rpc/RpcServer.h
+++ src/Rpc/RpcServer.h
@@ -18,7 +18,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
 
-#pragma once
 
 #include "HttpServer.h"
 
