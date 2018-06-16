e4c4771f557736bb447ad84d169273571a3b9773
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-06-16 20:43:45 +0430

    Fix int/int division

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -560,7 +560,7 @@ namespace CryptoNote {
 
 		// Keep LWMA sane in case something unforeseen occurs.
 		if (static_cast<int64_t>(boost::math::round(LWMA)) < T / 4)
-			LWMA = static_cast<double>(T / 4);
+			LWMA = static_cast<double>(T) / 4;
 
 		harmonic_mean_D = N / sum_inverse_D * adjust;
 		nextDifficulty = harmonic_mean_D * T / LWMA;
