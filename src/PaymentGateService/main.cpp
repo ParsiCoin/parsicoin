57ed0c737dc164c4bf2d87afb5e3c63c1ebc6fba
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-06-27 15:53:09 +0430

    Update main.cpp

--- src/PaymentGateService/main.cpp
+++ src/PaymentGateService/main.cpp
@@ -24,7 +24,7 @@
 #include "PaymentGateService.h"
 #include "version.h"
 
-#ifdef WIN32
+#ifdef _WIN32
 #include <windows.h>
 #include <winsvc.h>
 #else
@@ -40,7 +40,7 @@
 
 PaymentGateService* ppg;
 
-#ifdef WIN32
+#ifdef _WIN32
 SERVICE_STATUS_HANDLE serviceStatusHandle;
 
 std::string GetLastErrorMessage(DWORD errorMessageID)
@@ -131,7 +131,7 @@ int daemonize() {
 #endif
 
 int runDaemon() {
-#ifdef WIN32
+#ifdef _WIN32
 
   SERVICE_TABLE_ENTRY serviceTable[] {
     { "Payment Gate", serviceMain },
@@ -167,7 +167,7 @@ int runDaemon() {
 }
 
 int registerService() {
-#ifdef WIN32
+#ifdef _WIN32
   Logging::LoggerRef logRef(ppg->getLogger(), "ServiceRegistrator");
 
   char pathBuff[MAX_PATH];
@@ -224,7 +224,7 @@ int registerService() {
 }
 
 int unregisterService() {
-#ifdef WIN32
+#ifdef _WIN32
   Logging::LoggerRef logRef(ppg->getLogger(), "ServiceDeregistrator");
 
   SC_HANDLE scManager = NULL;
