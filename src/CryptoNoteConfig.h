d9719ef01a0e4255be5e7bc9f4a6dec82e995c9f
ParsiCoin (PARS) <36328859+ParsiCoin@users.noreply.github.com>
2018-10-05 06:54:57 +0330

    seednode update

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -114,8 +114,8 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const char* const SEED_NODES[] = { 
-  "seednode.parsicoin.net:17230",
-  "bg.parsicoin.net:17230",
+  "node.parsicoin.net:17230",
+  "explorer.parsicoin.net:17230",
 };
 
 struct CheckpointData {
