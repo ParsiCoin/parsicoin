0aa0b0aa683239e1682d1798493f93ae66f5165f
parsicoin <parsicoin@gmail.com>
2018-10-26 16:23:00 +0330

    v1.1.1

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -139,6 +139,7 @@ const std::initializer_list<CheckpointData> CHECKPOINTS = {
   {120000,  "4732064b59ca2aeeb7cb9339ce307268be3485a11617eed6de4745087abcb623" },
   {130001,  "c79b511759bbb277e6941e3b1a7e3301185cb7e81e7b604b7646867affcacfd2" },
   {140000,  "25898159bf209d441e1c9acec678cfa901de73e0d9208d5fc8a3e3267cee2c3b" },
+  {150000,  "70d277ef0b2a2ea1b72bd1e87c5ed81a650e972dc977c03e299d3a09ce7ec7e3" },
 };
 
 } // CryptoNote
