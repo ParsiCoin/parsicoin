27d5e235fe95f3106f74b4870b90c8d4deb413a4
ParsiCoin <parsicoin@gmail.com>
2019-11-29 03:39:16 +0330

    Check locked amounts in reserve proof RPC method

--- src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -1163,11 +1163,13 @@ struct K_COMMAND_RPC_CHECK_RESERVE_PROOF {
 		bool good;
 		uint64_t total;
 		uint64_t spent;
+		uint64_t locked;
 
 		void serialize(ISerializer &s) {
 			KV_MEMBER(good)
 			KV_MEMBER(total)
 			KV_MEMBER(spent)
+			KV_MEMBER(locked)
 		}
 	};
 };
