2e1f2a2fb91172ec941ab724571804741bc210af
ParsiCoin <parsicoin@gmail.com>
2020-04-24 20:50:57 +0430

    Refactoring parseUrlAddress util

--- src/Common/UrlTools.cpp
+++ src/Common/UrlTools.cpp
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
+#include <boost/regex.hpp>
 #include "CryptoNoteConfig.h"
 
 namespace Common {
@@ -29,63 +30,35 @@ const uint16_t HTTPS_PORT = 443;
 const std::string RPC_PATH = "/";
 
 bool parseUrlAddress(const std::string& url, std::string& host, uint16_t& port, std::string& path, bool& ssl) {
-  size_t pos = url.find("://");
-  size_t hostStart = 0;
-
-  host.clear();
-  port = 0;
-  path.clear();
-  ssl = false;
-
   bool res = true;
 
-  if (pos != std::string::npos) {
-    hostStart = pos + 3;
-  }
+  host.clear();
+  path.clear();
+  ssl = false;
+  port = 0;
 
-  size_t hostEnd = url.find(':', hostStart);
-
-  if (hostEnd != std::string::npos) {
-    size_t portEnd = url.find('/', hostEnd);
-    if (portEnd != std::string::npos) {
-      portEnd = portEnd - hostEnd - 1;
+  boost::regex uri_exp("^(https://|http://|)(([a-z|A-Z|0-9]|[a-z|A-Z|0-9]-[a-z|A-Z|0-9]|[a-z|A-Z|0-9]\\.)+)(:[0-9]{1,5}|)(/([\\w|-]+/)+|/|)$");
+  boost::cmatch reg_res;
+  if (boost::regex_match(url.c_str(), reg_res, uri_exp)) {
+    if (reg_res.length(4) > 0) {
+      int port_src = 0;
+      if (sscanf(reg_res.str(4).c_str() + 1, "%d", &port_src) == 1) {
+        if (port_src > 0 && port_src <= 0xFFFF) port = (uint16_t) port_src;
+      }
     } else {
-      portEnd = url.length() - 1;
+      if (strcmp(reg_res.str(1).c_str(), "http://") == 0) port = HTTP_PORT;
+      else if (strcmp(reg_res.str(1).c_str(), "https://") == 0) port = HTTPS_PORT;
+      else port = CryptoNote::RPC_DEFAULT_PORT;
     }
-    //port = Common::fromString<uint16_t>(url.substr(hostEnd + 1, portEnd));
-    int port_src = 0;
-    if (sscanf(url.substr(hostEnd + 1, portEnd).c_str(), "%d", &port_src) == 1) {
-      if (port_src > 0 && port_src <= 0xFFFF) port = (uint16_t) port_src;
+    if (port != 0) {
+      if (strcmp(reg_res.str(1).c_str(), "https://") == 0) ssl = true;
+      host.assign(reg_res[2].first, reg_res[2].second);
+      path.assign(reg_res[5].first, reg_res[5].second);
+      if (path.empty()) path = RPC_PATH;
+    } else {
+      res = false;
     }
   } else {
-    hostEnd = url.find('/', hostStart);
-    if (hostEnd == std::string::npos) hostEnd = url.length();
-    if (url.compare(0, 7, "http://") == 0) {
-      port = HTTP_PORT;
-    } else if (url.compare(0, 8, "https://") == 0) {
-      port = HTTPS_PORT;
-    } else {
-      port = CryptoNote::RPC_DEFAULT_PORT;
-    }
-  }
-
-  if (url.compare(0, 8, "https://") == 0) ssl = true;
-
-  host = url.substr(hostStart, hostEnd - hostStart);
-
-  size_t PathStart = url.find('/', hostEnd);
-  size_t PathEnd = url.find_last_of('/');
-  if (PathStart != std::string::npos && PathEnd != std::string::npos && PathStart != PathEnd) {
-    path = url.substr(PathStart, PathEnd - PathStart + 1);
-  } else {
-    path = RPC_PATH;
-  }
-
-  if (host.empty() || port == 0 || path.empty()){
-    host.clear();
-    port = 0;
-    path.clear();
-    ssl = false;
     res = false;
   }
 
